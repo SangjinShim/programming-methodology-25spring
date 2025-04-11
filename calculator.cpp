813ebc0038a20731d61b573495f5d0d2ce6eef07
SangjinShim <simki05@snu.ac.kr>
2025-04-11 12:58:33 +0900

    tdd-calculator

--- calculator.cpp
+++ calculator.cpp
@@ -3,16 +3,24 @@
 
 int Calculator::add(int a, int b) {
     // TODO
+    return a+b;
 }
 
 int Calculator::sub(int a, int b) {
     // TODO
+    return a-b;
 }
 
 int Calculator::mul(int a, int b) {
-    // TODO
+    long long result = 1LL * a * b; // promote to 64-bit
+    if (result > INT_MAX || result < INT_MIN) {
+        throw std::overflow_error("Multiplication result out of int range");
+    }
+    return static_cast<int>(result);
 }
+
 
 int Calculator::div(int a, int b) {
     // TODO
+    return a/b;
 }
